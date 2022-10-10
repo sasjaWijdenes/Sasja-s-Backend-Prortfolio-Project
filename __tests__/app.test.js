5653854b0fa3fe696f5245415b84a0954de365fd
sasjaWijdenes <saswij88@gmail.com>
2022-10-10 14:29:11 +0100

    added test for reviews/review_id but am going to ammend and merge my previous code first

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -36,6 +36,31 @@ describe("/api", () => {
       });
     });
   });
+  describe("/reviews", () => {
+    describe("/review_id", () => {
+      test("Should return a review object relating to the passed review_id", () => {
+        return request(app)
+          .get("/api/reviews/1")
+          .expect(200)
+          .then(({ body }) => {
+            const { review } = body;
+            console.log(review, "<<<review passed to test");
+            expect(review).toEqual({
+              review_id: 1,
+              title: "Culture a Love of Agriculture With Agricola ",
+              category: "strategy",
+              designer: "Uwe Rosenberg",
+              owner: "tickle122",
+              review_body: expect.any(String),
+              review_img_url:
+                "https://images.pexels.com/photos/4917821/pexels-photo-4917821.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260 ",
+              created_at: "2021-01-18 10:00:20.514",
+              votes: 1,
+            });
+          });
+      });
+    });
+  });
   describe("Error Handling", () => {
     test("404: Should return 404 code and handle error when passed an invalid route", () => {
       return request(app)
