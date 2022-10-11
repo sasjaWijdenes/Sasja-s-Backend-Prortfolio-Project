1d4c700259c72ffae256baf5c8976b0cd6717bc6
sasjaWijdenes <saswij88@gmail.com>
2022-10-11 15:47:16 +0100

    refators comment_count conversion to user sql query

--- db/models/reviews.models.js
+++ db/models/reviews.models.js
@@ -3,19 +3,16 @@ const db = require("../connection.js");
 exports.fetchReviewById = (id) => {
   return db
     .query(
-      `SELECT reviews.*, COUNT(comments.review_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id;`,
+      `SELECT reviews.*, COUNT(comments.review_id)::INT AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id;`,
       [id]
     )
     .then(({ rows }) => {
       const review = rows[0];
-      if (!review) {
-        return Promise.reject({
-          status: 404,
-          msg: `No user found for user ${id}`,
-        });
-      } else {
-        review.comment_count = Number(review.comment_count);
-        return review;
-      }
+      return !review
+        ? Promise.reject({
+            status: 404,
+            msg: `No user found for user ${id}`,
+          })
+        : review;
     });
 };
