53e2dd757c1af86883789831ceb48275ded43e99
sasjaWijdenes <saswij88@gmail.com>
2022-10-12 11:38:02 +0100

    still sorting out my git branches

--- db/models/reviews.models.js
+++ db/models/reviews.models.js
@@ -1,6 +1,6 @@
 const db = require("../connection.js");
 
-exports.fetchAllReviews = () => {
+exports.fetchAllReviews = (sort) => {
   return db
     .query(
       `SELECT reviews.*, COUNT(comments.review_id)::INT AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id GROUP BY reviews.review_id;`
