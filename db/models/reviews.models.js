d70fdf1522ca8de5658945d8c33917e1ed8ea7af
sasjaWijdenes <saswij88@gmail.com>
2022-10-13 10:20:25 +0100

    updated main

--- db/models/reviews.models.js
+++ db/models/reviews.models.js
@@ -1,62 +1,46 @@
 const db = require("../connection.js");
 
-exports.fetchAllReviews = (sort) => {
+exports.fetchAllReviews = (category) => {
   const queryValues = [],
     categories = ["euro game", "dexterity", "social deduction", undefined];
   let queryString = `SELECT reviews.*, COUNT(comments.review_id)::INT AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id `;
-  if (!categories.includes(sort))
+  if (!categories.includes(category))
     return Promise.reject({ status: 404, msg: "No such category" });
-  if (sort) {
+  if (category) {
     queryString += `WHERE reviews.category = $1 `;
-    queryValues.push(sort);
+    queryValues.push(category);
   }
   queryString += `GROUP BY reviews.review_id;`;
-  return db
-    .query(queryString, queryValues)
-    .then(({ rows: reviews }) => {
-      return reviews;
-    })
-    .catch((err) => console.log(err));
+  return db.query(queryString, queryValues).then(({ rows: reviews }) => {
+    return reviews;
+  });
 };
 
 exports.fetchReviewById = (id) => {
   return db
-    .query(`SELECT * FROM reviews WHERE review_id = $1`, [id])
-    .then(({ rows }) => {
-      const review = rows[0];
-      if (!review) {
-        return Promise.reject({
-          status: 404,
-          msg: `No user found for user ${id}`,
-        });
-      } else {
-        return review;
-      }
-    });
-};
-exports.updateReviewVotes = (id, votesToAdd) => {
-  if (!votesToAdd)
-    return Promise.reject({ status: 404, msg: "Passed malformed body" });
-  return db
     .query(
-      `UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING *;`,
-      [votesToAdd, id]
+      `SELECT reviews.*, COUNT(comments.review_id)::INT AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id;`,
+      [id]
     )
     .then(({ rows }) => {
       const review = rows[0];
-      if (!review) {
-        return Promise.reject({
-          status: 404,
-          msg: `No user found for user ${id}`,
-        });
-      } else {
-        return review;
-      }
+      return !review
+        ? Promise.reject({
+            status: 404,
+            msg: `No user found for user ${id}`,
+          })
+        : review;
     });
 };
+
 exports.updateReviewVotes = (id, votesToAdd) => {
   if (!votesToAdd)
     return Promise.reject({ status: 404, msg: "Passed malformed body" });
+  if (typeof votesToAdd !== "number")
+    return Promise.reject({
+      status: 400,
+      msg: "Request body has property of wrong data type.",
+    });
   return db
     .query(
       `UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING *;`,
